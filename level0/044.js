410fd3eeca677daa1194ab9e94d32aa72ce81dfd
Boyoung Lee <osoon9295@gmail.com>
2024-11-15 09:33:58 +0900

    044.js level0 문자열 겹쳐쓰기

--- level0/044.js
+++ level0/044.js
@@ -1,8 +1,11 @@
 // 문자열 겹쳐쓰기
 
 function solution(my_string, overwrite_string, s) {
-  var answer = "";
-  return answer;
+  return (
+    my_string.slice(0, s) +
+    overwrite_string +
+    my_string.slice(s + overwrite_string.length)
+  );
 }
 
-console.log(solution("He11oWor1d", "lloWorl", 2));
+console.log(solution("Program29b8UYP", "merS123", 7));
