bbabdaea2adee8829cddba9e6e71d3f330ac9b72
Boyoung Lee <osoon9295@gmail.com>
2024-11-25 10:44:37 +0900

    051 level0 세로 읽기

---
+++ level0/051.js
@@ -0,0 +1,11 @@
+// 세로 읽기
+
+function solution(my_string, m, c) {
+  let answer = "";
+  for (let i = c - 1; i < my_string.length; i += m) {
+    answer += my_string[i];
+  }
+  return answer;
+}
+
+console.log(solution("programmers", 1, 1));
