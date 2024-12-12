2df13bf0538d9d6738b3735fc6a67fda77965664
Boyoung Lee <osoon9295@gmail.com>
2024-12-12 15:53:40 +0900

    067 level0 카운트 다운

---
+++ level0/067.js
@@ -0,0 +1,11 @@
+// 카운트 다운
+
+function solution(start_num, end_num) {
+  var answer = [];
+  for (let i = start_num; i >= end_num; i--) {
+    answer.push(i);
+  }
+  return answer;
+}
+
+console.log(solution(10, 3));
