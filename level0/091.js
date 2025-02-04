26c0248a83664e090b54687303b8be469c73b78b
Boyoung Lee <osoon9295@gmail.com>
2025-02-04 22:21:00 +0900

    091 level0 주사위 게임 2

---
+++ level0/091.js
@@ -0,0 +1,15 @@
+// 주사위 게임 2
+
+function solution(a, b, c) {
+  if (a != b && b != c && c != a) {
+    return a + b + c;
+  } else if (a === b && b === c && c === a) {
+    return (
+      (a + b + c) * (a ** 2 + b ** 2 + c ** 2) * (a ** 3 + b ** 3 + c ** 3)
+    );
+  } else {
+    return (a + b + c) * (a ** 2 + b ** 2 + c ** 2);
+  }
+}
+
+console.log(solution(2, 6, 1));
