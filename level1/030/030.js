93d43ba1422154f3113c23d9ae3a0ff5fb3d6b8c
Boyoung Lee <osoon9295@gmail.com>
2025-07-03 15:20:02 +0900

    030 level1 소수 찾기

---
+++ level1/030/030.js
@@ -0,0 +1,20 @@
+// 소수 찾기
+
+function solution(n) {
+  let arr = Array(n + 1)
+    .fill(true)
+    .fill(false, 0, 2);
+
+  for (let i = 2; i * i <= n; i++) {
+    if (arr[i]) {
+      for (let j = i * i; j <= n; j += i) {
+        arr[j] = false;
+      }
+    }
+  }
+  console.log(arr);
+
+  return arr.filter((e) => e).length;
+}
+
+console.log(solution(5));
